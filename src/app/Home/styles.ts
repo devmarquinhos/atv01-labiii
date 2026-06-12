b7c0c08526fba652623e3a61257f285ed1633b93
devmarquinhos <marcos.emanuel.04@gmail.com>
2026-06-12 14:22:37 -0300

    first commit

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#d0d2d8',
+        alignItems: "center",
+        paddingTop: 62,
+    },
+    logo: {
+        maxWidth: 134,
+        maxHeight: 34,
+    },
+    form: {
+        width: "100%",
+        paddingHorizontal: 16,
+        gap: 10,
+        marginTop: 42,
+    },
+    content: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: "#FFFFFF",
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        padding: 24,
+        marginTop: 24,
+    }
+})
---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,23 @@
+import {TouchableOpacity, TouchableOpacityProps, Text} from "react-native"
+
+import { FilterStatus } from "@/types/FilterStatus";
+
+import { CircleCheck, CircleDashed } from "lucide-react-native";
+import { styles } from "./styles";
+import { StatusIcon } from "../StatusIcon";
+
+type Props = TouchableOpacityProps & {
+    status: FilterStatus,
+    isActive: boolean,
+}
+
+export function Filter({ status, isActive, ...rest }: Props) {
+    return(
+        <TouchableOpacity style={[styles.container, { opacity: isActive ? 1 : 0.5 }]} { ...rest }>
+            <StatusIcon status={status} />
+            <Text style={styles.title}>
+                { status === FilterStatus.DONE ? "Comprados" : "Pendentes"  }
+            </Text>
+        </TouchableOpacity>
+    );
+}
